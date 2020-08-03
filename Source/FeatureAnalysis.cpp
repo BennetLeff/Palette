af7d8efce530069f2873749fa0d16ae7d1b665d7
Bennet Leff <bennetleff@gmail.com>
2020-08-03 15:02:46 -0500

    Testcases for RMS in FeatureAnalysis

--- Source/FeatureAnalysis.cpp
+++ Source/FeatureAnalysis.cpp
@@ -1,2 +1,7 @@
 #include "FeatureAnalysis.h"
 
+template <typename R>
+constexpr auto Palette::rootMeanSquare(const R& data)
+{
+	return 0;
+}
--- Source/FeatureAnalysis.h
+++ Source/FeatureAnalysis.h
@@ -1,9 +1,34 @@
 #pragma once
 
 #include <range/v3/all.hpp>
+#include "doctest.h"
 
 namespace Palette
 {
-	// template <std::ranges::range R>
-	// constexpr auto rootMeanSquare(const R&& data);
+	template <typename R>
+	constexpr auto rootMeanSquare(const R& data);
+}
+
+TEST_CASE("Palette::rootMeanSqure(const R&&)")
+{
+	std::vector<float> testData;
+
+	SUBCASE("RMS for empty range")
+	{
+		CHECK(Palette::rootMeanSquare(testData) == 0);
+	}
+
+	testData.push_back(1.0f);
+
+	SUBCASE("RMS for 1 element")
+	{
+		CHECK(Palette::rootMeanSquare(testData) == 1);
+	}
+
+	for (auto i = 2; i <= 10; i++) testData.push_back((float)i);
+
+	SUBCASE("RMS for range of 1-10")
+	{
+		CHECK(Palette::rootMeanSquare(testData) == 55.0f);
+	}
 }
