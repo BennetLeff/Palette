bf842503d864e897fb3be8699c4e87c9fa96e7d8
Bennet Leff <bennetleff@gmail.com>
2020-08-21 19:53:29 -0500

    Remove custom allocator, create methods for RMS & Peak Energy
    The custom allocator was not working. It's a reasonable optimization but I don't want to spend too much time on it before I get other things working.

--- Source/GrainClassifier.h
+++ Source/GrainClassifier.h
@@ -7,50 +7,6 @@
 
 namespace Palette 
 {
-    /*
-     * The PreAllocator allocator lets us instantiate a std::vector which points
-     * to preallocated memory. 
-     * 
-     * Each Grain holds data of the type juce::AudioBuffer which makes 
-     * interfacing with JUCE library code simpler as well as providing the
-     * utilities of the type. However, every method in the Gist library takes a
-     * std::vector. For performance reasons, we would like to use Gist methods
-     * without reallocating all the memory in each juce::AudioBuffer so we need
-     * a custom allocator.
-     */
-    template <typename T>
-    class PreAllocator
-    {
-    private:
-        T* memory_ptr;
-        std::size_t memory_size;
-
-    public:
-        typedef std::size_t size_type;
-        typedef T* pointer;
-        typedef T value_type;
-
-        PreAllocator(T* memory_ptr, std::size_t memory_size) : memory_ptr(memory_ptr), memory_size(memory_size) {}
-
-        PreAllocator(const PreAllocator& other) throw() : memory_ptr(other.memory_ptr), memory_size(other.memory_size) {};
-
-        template<typename U>
-        PreAllocator(const PreAllocator<U>& other) throw() : memory_ptr(other.memory_ptr), memory_size(other.memory_size) {};
-
-        template<typename U>
-        PreAllocator& operator = (const PreAllocator<U>& other) { return *this; }
-        PreAllocator<T>& operator = (const PreAllocator& other) { return *this; }
-
-        // Note that we don't free memory on destruction since this vector does
-        // not own the memory it refers to.
-        ~PreAllocator() {}
-
-        pointer allocate(size_type n, const void* hint = 0) { return memory_ptr; }
-        void deallocate(T* ptr, size_type n) {}
-
-        size_type max_size() const { return memory_size; }
-    };
-
 	template<typename SampleType>
 	class GrainClassifier
 	{
@@ -65,27 +21,29 @@ namespace Palette
 
 		const auto spectralCentroid(const Grain<SampleType>& grain) 
         { 
-            /*
-             * Construct a std::vector from the interal pointer in the 
-             * juce::AudioBuffer from within grain. This saves us from copying
-             * an entire grain worth of data (which would otherwise be copied 
-             * for each grain in the entire synth).
-             */
+			auto ptr = grain.sampleData.getReadPointer(0);
             
-            return gist.spectralCentroid(nonOwningVector(grain));
+			// We must construct a vector from the internal pointer inside the 
+			// juce::AudioBuffer inside the Grain because that's the only type
+			// Gist accepts.
+            return gist.spectralCentroid(std::vector<SampleType>(ptr, ptr + grain.sampleData.getNumSamples()));
         }
+
+		const auto rootMeanSquare(const Grain<SampleType>& grain)
+		{
+			auto ptr = grain.sampleData.getReadPointer(0);
+
+			return gist.rootMeanSquare(std::vector<SampleType>(ptr, ptr + grain.sampleData.getNumSamples()));
+		}
+
+		const auto peakEnergy(const Grain<SampleType>& grain)
+		{
+			auto ptr = grain.sampleData.getReadPointer(0);
+
+			return gist.peakEnergy(std::vector<SampleType>(ptr, ptr + grain.sampleData.getNumSamples()));
+		}
+
 	private:
 		Gist<SampleType> gist;
-
-        static const auto nonOwningVector = [](const Grain& grain)
-        {
-            return std::vector<SampleType, PreAllocator<SampleType>>(grain.sampleData.getReadPointer(0),
-                grain.sampleData.getNumSamples());
-        };
 	};
-}
-
-TEST_CASE("Palette::PreAllocator<SampleType>(SampleType*, std::size_t)")
-{
-
 }
